b96896b256d1efe292c98857987dfc0e895eb404
FPABacks <backsfpa@gmail.com>
2020-05-20 15:39:08 +0200

    Just to make sure we have the same version I hope it works.

--- main_RVMC.py
+++ main_RVMC.py
@@ -11,6 +11,7 @@ plt.switch_backend('QT4Agg')
 # np.random.seed(0)
 RV_errors = np.array([0.8, 2.3, 1.0, 1.8, 0.8, 1.0, 1.4, 2.5, 2.0, 0.4, 1.5, 0.6])
 
+
 def initialize_parameters(number_of_stars=100000, min_mass=6, max_mass=20, min_period=1.4, max_period=3500):
     """
     Create the random samples following their respective distributions.
@@ -21,36 +22,50 @@ def initialize_parameters(number_of_stars=100000, min_mass=6, max_mass=20, min_p
     :param max_period:          (scalar) maximum period in days.
     :return:    Note returns the masses in kg and period in seconds.
     """
-<<<<<<< HEAD
+# <<<<<<< HEAD
     inclination = np.arccos(np.random.random(size=number_of_stars))
-=======
-    inclination = np.random.uniform(0, 1, size=number_of_stars)# * np.pi * 0.5 #cosi = random(0,1)
-    inclination = np.arccos(inclination)
->>>>>>> b87fcb5dc52eee01ddf66b2f6134e958ddde04cb
-    a = 1.3  # IMF powerlaw index -1 because of sample method
-    primary_mass = np.random.uniform(min_mass**(-a), max_mass**(-a), size=number_of_stars) ** -(1. / a) * 2e30
+# =======
+#     inclination = np.random.uniform(0, 1, size=number_of_stars)# * np.pi * 0.5 #cosi = random(0,1)
+#     inclination = np.arccos(inclination)
+# >>>>>>> b87fcb5dc52eee01ddf66b2f6134e958ddde04cb
+    a = 2.3  # IMF powerlaw index -1 because of sample method
+    # primary_mass = np.random.uniform(min_mass**(-a), max_mass**(-a), size=number_of_stars) ** -(1. / a) * 2e30
+
+    # CDF sampling of the IMF powerlaw
+    Pmin = (min_mass / min_mass) ** (-a + 1)
+    Pmax = (max_mass / min_mass) ** (-a + 1)
+    P = np.random.uniform(Pmax, Pmin, size=number_of_stars)
+    primary_mass = P ** (1. / (-a + 1)) * min_mass  * 2e30
+
 
     period = 10**(np.random.uniform(np.log10(min_period)**0.5, np.log10(max_period)**0.5, number_of_stars)**2) * 24 * 3600
     #Oepik distribution
     # period = 10**(np.random.uniform(np.log10(min_period), np.log10(max_period), number_of_stars)) * 24 * 3600
 
-    mass_ratio = np.random.uniform(0.1, 1, number_of_stars)
+    mass_ratio = np.random.uniform(0.1, 1., number_of_stars)
     # In case you want a mass ratio distribution as f(q) ~ q**-0.1
     # mass_ratio = np.random.uniform(0.1 ** (1. / 1.1), 1, number_of_stars) ** 1.1
 
     orbit_rotation = np.random.uniform(0, 1, size=number_of_stars) * 2 * np.pi
 
-    eccentricity = (np.random.uniform(0**0.5, 0.9**0.5, number_of_stars)**2)
+    eccentricity = (np.random.uniform(0**0.5, 1.**0.5, number_of_stars)**2)
     # Edit eccentricity array to avoid unphysical combinations of eccentricity and period
     # All orbits with periods of 4 or less days are circularized e=0
     # If the period is between 4 and 6 days and the eccentricity is high, divide it in half to better repesent the
     # observed distribution
-    index4d = np.where(period <= 4 * 24 * 3600)
-    index6d = np.where(period[np.where(eccentricity[np.where((period > 4 * 24 * 3600) &
-                                                             (period < 6 * 24 * 3600))])] > 0.9)
+    # index4d = np.where(period <= 4 * 24 * 3600)
+    # index6d = np.where(period[np.where(eccentricity[np.where((period > 4 * 24 * 3600) &
+    #                                                          (period < 6 * 24 * 3600))])] > 0.9)
+
+    index4d = period <= (4 * 24 * 3600)
+    index6d = (period > (4 * 24 * 3600)) & (period < (6 * 24 * 3600))
+    index09e = (eccentricity > 0.9)
+
     eccentricity[index4d] = 0.
     for i6d in index6d:
         eccentricity[i6d] = eccentricity[i6d] / 2.
+        
+    eccentricity[index09e] = eccentricity[index09e] / 2.
 
     time = np.random.uniform(0, 1, size=number_of_stars) * period
     eccentric_anomaly = find_eccentric_anomaly(eccentricity, time, period)
@@ -566,6 +581,7 @@ def simple_std_plot_bigSample(number_of_samples=10**5, sample_size=12, big_sampl
     plt.savefig("sigma1D_test_fast1.pdf")
     plt.show()
 
+
 def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
@@ -714,8 +730,8 @@ def find_best_period(pmin=1.4, pmax=5500, Npoints=100, measured_errors=RV_errors
 
 
 if __name__ == '__main__':
-
-    s# find_best_period()
+    simple_std_plot_bigSample()
+    # find_best_period()
 
     # simple_std_plot_bigSample(measured_errors=RV_errors, min_mass=6)
 
